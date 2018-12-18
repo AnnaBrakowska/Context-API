88794a7de158ff85b7339dcff1b9dc41bc722902
Anna <anna.brakowska91@gmail.com>
2018-12-18 18:14:58 -0500

    added basic context api setup

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import "./App.css";
+const FirstContext = React.createContext();
+
+class MyProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fruits: ["bananas", "oranges", "apples"],
+      vegetables: ["tomato", "cucumber", "lettuce"]
+    };
+  }
+
+  render() {
+    return (
+      <FirstContext.Provider value={{ state: this.state }}>
+        {this.props.children}
+      </FirstContext.Provider>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <FruitBox />
+          <VeggieBox />
+        </div>
+      </MyProvider>
+    );
+  }
+}
+
+const FruitBox = props => {
+  return (
+    <div>
+      <h1>I am a fruit box</h1>
+      <ul>
+        <FirstContext.Consumer>
+          {context =>
+            context.state.fruits.map((fruit, index) => (
+              <li key={`fruit-${index}`}>{fruit}</li>
+            ))
+          }
+        </FirstContext.Consumer>
+      </ul>
+    </div>
+  );
+};
+
+const VeggieBox = props => {
+  return (
+    <div>
+      <h1>I am a veggie Box</h1>
+      <ul>
+        <FirstContext.Consumer>
+          {context =>
+            context.state.vegetables.map((veggie, index) => (
+              <li key={`veggie-${index}`}>{veggie}</li>
+            ))
+          }
+        </FirstContext.Consumer>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
